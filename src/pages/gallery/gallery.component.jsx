243ac56d618080f5dee34f6204fdf9b53778c3eb
manoj <manojkarthikeya@gmail.com>
2022-05-01 01:22:34 +0530

    fixed warnings

--- src/pages/gallery/gallery.component.jsx
+++ src/pages/gallery/gallery.component.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import "./gallery.styles.scss";
-import { useSpring, animated } from "react-spring";
-import { useMove,useScroll } from "@use-gesture/react";
 const GalleryPage = () => {
-	const bind = useMove(({ direction, target }) => {
-		const [a,b] = direction;
-	});
-	return <animated.div {...bind()} className="main-gallery">
-		<div className="junk"></div>
-		<div className="junk"></div>
-		<div className="junk"></div>
-		<div className="junk"></div>
-		<div className="junk"></div>
-		<div className="junk"></div>
-	</animated.div>;
+	return <div/>
 };
 
 export default GalleryPage;
